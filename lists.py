fdb3f00b7b3ad14d6631f59033a83290c97fd189
charcoal <202420163@students.asu.edu.jo>
2026-03-18 09:24:24 +0300

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,23 @@
+# lists :)
+theList = ["acheron", "ahmad", "rafat", "dalbah"]
+print(type(theList))
+# notice: theList is considered an object, and therefore it has its own methods
+
+automobiles = ["car", "truck", "motorcycle"]
+print(len(automobiles))
+# notice: function 'len' returns the length of the list
+
+marks = [90, 79, 45, 87]
+print(marks)
+print(type(marks))
+
+secondList = ["rafat", 2.5, 10, False]
+# notice: lists are heterogenous, they can contain different data types
+
+print(secondList[0])
+print(type(secondList[3]))
+
+secondList[1] = 3.5
+print(secondList)
+# notice: lists are mutable, their content can be changed
+
