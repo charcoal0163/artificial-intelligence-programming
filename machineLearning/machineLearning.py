d83c4781c91c5e9cff6ae4156e59bc03cd9a4b0d
charcoal <202420163@students.asu.edu.jo>
2026-05-13 09:42:22 +0300

    Add files via upload

---
+++ machineLearning/machineLearning.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# step 1: read from the CSV file
+dataframe = pd.read_csv(r"C:\Users\ASU\Documents\temporary file (0163)\machineLearning\weather_forecast.csv")
+print(dataframe)
+
+# step 2: select the output column
+y = dataframe["Play"]
+print(y)
+
+# step 3: select the feature columns
+# # way 1:
+# x = dataframe[["Outlook", "Temperature", "Humidity", "Windy"]]
+# way 2:
+x = dataframe.iloc[:, 0:4]
+print(x)
